34a22b66f8344e1f5c021fcfb44f5a55b3046e89
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-29 19:23:46 +0600

    auto commit Wed Oct 29 19:23:46 +06 2025

---
+++ autocommit.sh
@@ -0,0 +1,16 @@
+# PATH="./GH_Computer_systems_and_industrial_programming-main"
+# cd "$PATH" || exit
+
+
+echo "Control all changes in $PATH..."
+
+while true; do
+    if ! git diff --quiet || ! git diff --cached --quiet; then
+		echo "Changes defined! Commit and push..."
+		git add .
+		git commit -m "auto commit $(date)" || true
+		git push
+    fi
+    sleep 5
+done
+
