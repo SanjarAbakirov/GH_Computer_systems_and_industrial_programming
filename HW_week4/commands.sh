8119b1b185c0c3a01538cdaf88ccf46bbc7ce3cb
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-03 14:23:01 +0600

    auto commit Mon Nov  3 14:23:01 +06 2025

--- HW_week4/commands.sh
+++ HW_week4/commands.sh
@@ -2,4 +2,4 @@ name="Sanjar"
 echo "Hello, $name!"
 echo 'Hello, $name!' # single quotes do not expand variables
 
-echo "I'm a programmer. $(pwd) is my directory."
+echo "I'm in $(pwd) this directory."
