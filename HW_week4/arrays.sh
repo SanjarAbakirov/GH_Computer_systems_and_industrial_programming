5be097be00e3f77478eb4d9955bb65e0724d9314
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-03 14:10:25 +0600

    new array

---
+++ HW_week4/arrays.sh
@@ -0,0 +1,17 @@
+array=(you will be the best programmer)
+
+# echo ${array[5]}  # Output: best
+# echo ${array[@]}  # Output: you will be the best programmer
+# echo ${#array[@]} # Output: 6
+
+function foo()
+{
+    echo "Arguments passed to script: $@"
+    echo "And: $1 $2..."
+    echo "This is a function"
+    returnValue=0
+    return $returnValue
+}
+
+foo arg1 arg2
+returnValue=$?
