b06e803690d252a4853665299bb937376304c83b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-03 14:39:11 +0600

    auto commit Mon Nov  3 14:39:11 +06 2025

--- HW_week4/commands.sh
+++ HW_week4/commands.sh
@@ -8,3 +8,6 @@ echo "I'm here `pwd` too."  # older syntax using backticks
 while read -r line; do
     echo "Line: $line"
 done < HW_week4/commands.sh
+
+printf "Hello %s, I'm %s " Sven Olga 
+printf "%.2f\n" 3.14159
