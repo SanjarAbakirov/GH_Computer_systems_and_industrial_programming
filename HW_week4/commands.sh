ed45240dd17db717a9e3c876a0653ee60b2c148d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-03 14:37:34 +0600

    auto commit Mon Nov  3 14:37:34 +06 2025

--- HW_week4/commands.sh
+++ HW_week4/commands.sh
@@ -4,3 +4,7 @@ echo 'Hello, $name!' # single quotes do not expand variables
 
 echo "I'm in $(pwd) this directory."
 echo "I'm here `pwd` too."  # older syntax using backticks
+
+while read -r line; do
+    echo "Line: $line"
+done < HW_week4/commands.sh
