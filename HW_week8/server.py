97399b5b50a51d077735399586052a278592d9aa
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-20 00:08:18 +0600

    auto commit Thu Nov 20 00:08:18 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -4,7 +4,7 @@ import threading  # multiple way to create multipule threads in one pipe line pr
 PORT = 5050
 # SERVER = "192.168.31.127"
 SERVER = socket.gethostbyname(socket.gethostname())
-
+# means get the ip adress of this computer
 
 # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # stok_steam means we are using TCP - transmission contro protocol
