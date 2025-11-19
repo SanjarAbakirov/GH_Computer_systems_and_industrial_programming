757a4b8c7f1297dc57310973d0df359b357dee5a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-19 23:48:53 +0600

    auto commit Wed Nov 19 23:48:53 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -1,5 +1,9 @@
 import socket
 import threading  # multiple way to create multipule threads in one pipe line program
+import time
+
+time.sleep(1)
+print("helloы")
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # stok_steam means we are using TCP - transmission contro protocol
