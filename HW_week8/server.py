1dbbf1138a0c88a38b1fbff3306ed7d51b1ca84c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-20 22:32:15 +0600

    auto commit Thu Nov 20 22:32:15 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -32,7 +32,7 @@ def handle_client(conn, addr):
             if msg_length:
                 msg_length = int(msg_length)
                 msg = conn.recv(msg_length).decode(FORMAT)
-                if msg == DISCONNECT_MESSAGE:
+                if msg == DISCONECT_MESSAGE:
                     connected = False
 
             print(f"[{addr}] {msg}")  # handling the disconnection clearly
