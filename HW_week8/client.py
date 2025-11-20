bf2a1021a9066a480590688b1836ca402cc32d21
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-20 22:15:04 +0600

    auto commit Thu Nov 20 22:15:04 +06 2025

--- HW_week8/client.py
+++ HW_week8/client.py
@@ -5,6 +5,7 @@ PORT = 5050
 FORMAT = 'UTF-8'
 DISCONECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.1.26"
+ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect()
