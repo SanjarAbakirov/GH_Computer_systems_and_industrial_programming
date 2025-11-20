a31253d01eb526982e5dbd4d5ed542ee6bb72819
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-20 22:06:13 +0600

    auto commit Thu Nov 20 22:06:13 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -34,6 +34,7 @@ def handle_client(conn, addr):
                 connected = False
 
             print(f"[{addr}] {msg}")  # handling the disconnection clearly
+
         conn.close()  # closed disconnection
 
 
