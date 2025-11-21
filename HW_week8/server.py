e395f342cbeeaf6f18e3eb2dffcc0a89494ea9bd
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-21 20:35:46 +0600

    auto commit Fri Nov 21 20:35:46 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -15,7 +15,7 @@ DISCONECT_MESSAGE = "!DISCONNECT"
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# what type of IP adress we are looking for
+# AF_INET - what type of IP adress we are looking for
 server.bind(ADDR)  # bound socke to this adress
 
 
