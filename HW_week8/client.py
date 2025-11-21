6b793fe4b408e5012b4f9fb715921620d4194b1a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-21 23:44:07 +0600

    auto commit Fri Nov 21 23:44:07 +06 2025

--- HW_week8/client.py
+++ HW_week8/client.py
@@ -4,7 +4,7 @@ HEADER = 64  # 64 TELLS message that is going to come next
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.26"
+SERVER = "192.168.31.127"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
