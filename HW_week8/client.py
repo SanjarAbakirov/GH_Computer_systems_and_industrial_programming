257a0b75ef365181b405acefa76c79a116ddfcd7
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-24 00:03:57 +0600

    auto commit Mon Nov 24 00:03:57 +06 2025

--- HW_week8/client.py
+++ HW_week8/client.py
@@ -16,3 +16,5 @@ def send(msg):
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
