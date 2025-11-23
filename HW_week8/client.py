8de8956976a606f77e65d2031cc1c5a51e509701
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-24 00:00:17 +0600

    encode

--- HW_week8/client.py
+++ HW_week8/client.py
@@ -14,4 +14,4 @@ client.connect(ADDR)
 def send(msg):
     message = msg.encode(FORMAR)
     msg_length = len(message)
-    send_length = str(msg_length)
+    send_length = str(msg_length).encode(FORMAT)
