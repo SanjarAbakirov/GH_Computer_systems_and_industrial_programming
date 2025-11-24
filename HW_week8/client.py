3782538ec8d1e3d244e16311cc2dfc83bd6f1ea0
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-24 23:51:02 +0600

    auto commit Mon Nov 24 23:51:02 +06 2025

--- HW_week8/client.py
+++ HW_week8/client.py
@@ -3,7 +3,7 @@ import socket
 HEADER = 64  # 64 TELLS message that is going to come next
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONECT_MESSAGE = "!DISCONNECT check out the network"
+DISCONECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.31.127"
 ADDR = (SERVER, PORT)
 
