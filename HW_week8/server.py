fb93956eb252fe603d804ff8f5389815e9099986
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-30 21:09:03 +0600

    auto commit Sun Nov 30 21:09:03 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -17,7 +17,7 @@ server.bind(ADDR)  # bound socket to this adress (SERVER, PORT)
 
 
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    print(f"[Warning! NEW CONNECTION] {addr} connected.")
     while True:
         # we will not pass this kind of code untill we receive the message from client
         msg_length = conn.recv(HEADER).decode(FORMAT)
