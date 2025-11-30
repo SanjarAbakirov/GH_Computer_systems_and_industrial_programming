8ce30d63daba3787440fc2143ce6111e6936bdfe
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-30 21:11:58 +0600

    auto commit Sun Nov 30 21:11:58 +06 2025

--- HW_week8/server.py
+++ HW_week8/server.py
@@ -20,7 +20,6 @@ def handle_client(conn, addr):
     print(f"[Warning! NEW CONNECTION] {addr} connected.")
     connected = True
     while connected:
-        # we will not pass this kind of code untill we receive the message from client
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
