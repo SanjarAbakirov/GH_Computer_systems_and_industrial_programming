6f0f44bc740df41ee418d292a897caa0a8766e5d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-14 01:03:51 +0600

    auto commit Sun Dec 14 01:03:51 +06 2025

--- client_server/server.py
+++ client_server/server.py
@@ -4,7 +4,7 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP connection
 server.bind(('0.0.0.0', 9999))  # automatically
 
-server.listen(5)  # how many connection are alllowed
+# server.listen(5)  # how many connection are alllowed
 
 while True:
     client, addr = server.accept()
