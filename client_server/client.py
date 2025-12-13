ef93719726db373980d4e19689b702aab5ce5345
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-14 01:05:49 +0600

    auto commit Sun Dec 14 01:05:49 +06 2025

--- client_server/client.py
+++ client_server/client.py
@@ -1,6 +1,7 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP connection
 client.connect(('127.0.0.1', 9999))
 
 client.send(f"Hello from client".encode())
