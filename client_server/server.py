024756af21a39914b292b1033c88597312d72d8f
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-14 00:51:18 +0600

    auto commit Sun Dec 14 00:51:18 +06 2025

--- client_server/server.py
+++ client_server/server.py
@@ -1,6 +1,6 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP connection
 server.bind(('0.0.0.0', 9999))
 
 server.listen(5)
