302e3e8792783c5e7cd51ce7839ed13e42c62fd4
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-14 01:01:54 +0600

    auto commit Sun Dec 14 01:01:54 +06 2025

--- client_server/client.py
+++ client_server/client.py
@@ -4,4 +4,4 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 9999))
 
 client.send(f"Hello from client".encode())
-print(client.recv(1024))
+print(client.recv(1024).decode())
