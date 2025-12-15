ba016b8812d8a2cfa67da2a4b3ba6fd4368efb93
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-15 22:03:07 +0600

    auto commit Mon Dec 15 22:03:06 +06 2025

--- HW_week11/main.py
+++ HW_week11/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {}  # dictionary because we apply to JSON
+    return "Hello!"  # {}  # dictionary because we apply to JSON
 
 
 if __name__ == "__main__":
