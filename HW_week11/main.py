5977fee950a373de2451a5c9f8dcee574714a821
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-15 22:05:05 +0600

    auto commit Mon Dec 15 22:05:05 +06 2025

--- HW_week11/main.py
+++ HW_week11/main.py
@@ -1,12 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy  # ORM
 
 app = Flask(__name__)
 
 # Create Database
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"
 
 # Create Routes
+
 
 @app.route("/")
 def home():
