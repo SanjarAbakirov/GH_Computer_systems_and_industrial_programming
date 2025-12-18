fadddbb8481d2df77b1099fa1c416c0bc97ab925
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-18 23:33:01 +0600

    auto commit Thu Dec 18 23:33:01 +06 2025

--- practice/index.py
+++ practice/index.py
@@ -2,8 +2,8 @@
 import socket
 # host = socket.gethostbyname(socket.gethostname()) #dynamicly - if using virtual box - will be not good
 HOST = '192.168.31.127'
-HOST = '127.0.01'  # for local host
-HOST = 'Localhost'  # for local host
+# HOST = '127.0.0.1'  # for local host
+# HOST = 'Localhost'  # for local host
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
