37e0166b654b1d9defa44b413cd086d59dc60270
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-22 22:26:52 +0600

    auto commit Mon Dec 22 22:26:52 +06 2025

--- practice/endyear.py
+++ practice/endyear.py
@@ -1,14 +1,22 @@
 # anagram
 
-def is_anagram(str1, str2):
-    str1 = str1.replace(" ", "").lower()
-    str2 = str2.replace(" ", "").lower()
-    return sorted(str1) == sorted(str2)
+# def is_anagram(str1, str2):
+#     str1 = str1.replace(" ", "").lower()
+#     str2 = str2.replace(" ", "").lower()
+#     return sorted(str1) == sorted(str2)
 
 
-print(is_anagram("listen", "silent"))
+# print(is_anagram("listen", "silent"))
 
 # -----------------
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
+
+
+class Solution {
+    public:
+    vector < int > twoSum(vector < int > & nums, int target) {
+
+    }
+}
